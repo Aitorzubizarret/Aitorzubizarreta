efd1ddbc925924fd34255bfb654e3e41322d71d7
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2022-06-18 13:45:40 +0200

    Initial Commit

---
+++ Aitor Zubizarreta/Aitor_ZubizarretaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aitor_ZubizarretaApp.swift
+//  Aitor Zubizarreta
+//
+//  Created by Aitor Zubizarreta on 18/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Aitor_ZubizarretaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
