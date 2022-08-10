b36492f1cee3b1d5c472a04deafdcfdb54879ab6
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2022-08-10 17:30:46 +0200

    Deleted the SwiftUI project and created a new project with storyboard.

---
+++ aitorzubizarret/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aitorzubizarret
+//
+//  Created by Aitor Zubizarreta on 10/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
