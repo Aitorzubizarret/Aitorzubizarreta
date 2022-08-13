9de28fa5e7565af2c2dcdacdebbbd02a0bc5d615
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2022-08-13 12:29:59 +0200

    Updated the UI of the ContactMe cell.

--- aitorzubizarret/Cells/ContactMeCell/ContactMeTableViewCell.swift
+++ aitorzubizarret/Cells/ContactMeCell/ContactMeTableViewCell.swift
@@ -8,16 +8,38 @@
 import UIKit
 
 class ContactMeTableViewCell: UITableViewCell {
-
+    
+    // MARK: - UI Elements
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var arrowImageView: UIImageView!
+    
+    // MARK: - Properties
+    
+    // MARK: - Methods
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        setupView()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    private func setupView() {
+        self.selectionStyle = .none
+        
+        mainView.backgroundColor = UIColor.white
+        
+        mainView.layer.cornerRadius = 6
+        
+        mainView.layer.borderWidth = 1
+        mainView.layer.borderColor = UIColor(named: "myDarkBlue")!.cgColor
     }
     
 }
