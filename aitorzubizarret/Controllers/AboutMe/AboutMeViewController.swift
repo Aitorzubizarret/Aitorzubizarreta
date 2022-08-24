e044f7c8286acd2567d8dc98af249cab3100fc3c
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2022-08-24 22:31:14 +0200

    Added a tableview and some logic to "AboutMeViewController".

--- aitorzubizarret/Controllers/AboutMe/AboutMeViewController.swift
+++ aitorzubizarret/Controllers/AboutMe/AboutMeViewController.swift
@@ -8,22 +8,69 @@
 import UIKit
 
 class AboutMeViewController: UIViewController {
-
+    
+    // MARK: - UI Elements
+    
+    // MARK: - Properties
+    
+    private var tableView = UITableView()
+    
+    // MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        initTableView()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func initTableView() {
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Constraints.
+        let safeArea: UILayoutGuide = view.layoutMarginsGuide
+        
+        tableView.translatesAutoresizingMaskIntoConstraints =  false
+        
+        tableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        
+        tableView.separatorStyle = .none
+        
+        // Register cells.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-    */
+    
+}
 
+// MARK: - UITableView Delegate
+
+extension AboutMeViewController: UITableViewDelegate {}
+
+// MARK: - UITableView Data Source
+
+extension AboutMeViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = "Text"
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
 }
