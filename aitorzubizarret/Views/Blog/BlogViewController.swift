afa13a802ac16abcf3b3ab17523731ecd52c61f7
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2022-09-18 17:54:08 +0200

    Added a tableview and some logic to initialize it in "BlogViewController".

--- aitorzubizarret/Views/Blog/BlogViewController.swift
+++ aitorzubizarret/Views/Blog/BlogViewController.swift
@@ -13,16 +13,60 @@ class BlogViewController: UIViewController {
     
     // MARK: - Properties
     
+    private var tableView = UITableView()
+    
     // MARK: - Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         initView()
+        initTableView()
     }
     
     private func initView() {
         title = "Blog"
+    }
+    
+    private func initTableView() {
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Constraints.
+        let safeArea: UILayoutGuide = view.layoutMarginsGuide
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        tableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: safeArea.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: safeArea.leftAnchor).isActive = true
+        
+        tableView.separatorStyle = .none
+    }
+    
+}
+
+// MARK: - UITableView Delegate
+
+extension BlogViewController: UITableViewDelegate {}
+
+// MARK: - UITableView Data Source
+
+extension BlogViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
     
 }
