35b5d67388a44788441594eba862db57784d335c
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2022-09-26 23:47:49 +0200

    Added a couple of colors to Assets. Added the UI elements and the logic to "AppsHeaderTableViewCell" cell.

--- aitorzubizarret/Cells/Main/AppsHeaderCell/AppsHeaderTableViewCell.swift
+++ aitorzubizarret/Cells/Main/AppsHeaderCell/AppsHeaderTableViewCell.swift
@@ -7,17 +7,54 @@
 
 import UIKit
 
-class AppsHeaderTableViewCell: UITableViewCell {
+protocol AppsHeaderCellActions {
+    func goToAppsListVC()
+}
 
+class AppsHeaderTableViewCell: UITableViewCell {
+    
+    // MARK: - UI Elements
+    
+    @IBOutlet weak var headerTitleLabel: UILabel!
+    @IBOutlet weak var allAppsButton: UIButton!
+    @IBAction func allAppsButtonTapped(_ sender: Any) {
+        goToAppsListVC()
+    }
+    
+    // MARK: - Properties
+    
+    var delegate: AppsHeaderTableViewCell?
+    
+    // MARK: - Methods
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        initView()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    private func initView() {
+        selectionStyle = .none
+        
+        // Attributed Button Title.
+        let buttonText: String = "TODAS"
+        let font = UIFont(name: "Helvetica Neue Bold", size: 12.0)
+        let attributes = [NSAttributedString.Key.font: font]
+        let attributedButtonText = NSAttributedString(string: buttonText, attributes: attributes as [NSAttributedString.Key : Any])
+        
+        allAppsButton.setAttributedTitle(attributedButtonText, for: .normal)
+    }
+    
+    private func goToAppsListVC() {
+        guard let delegate = delegate else { return }
+        
+        delegate.goToAppsListVC()
     }
     
 }
